24b3cda13a8770d97426d2f5d47d76bda0295846
Harald Scheirich <hscheirich@yahoo.com>
2025-05-18 14:49:40 -0400

    Hard Swap of jewels

--- scripts/Nodes/BoardNode.cs
+++ scripts/Nodes/BoardNode.cs
@@ -14,13 +14,24 @@ public partial class BoardNode : Node2D
     [Export] Sprite2D SelectionSprite { get; set; }
 
     
+    private Vector2I? _selected;
+    
     private Common.Board _board;
     private Array2D<Sprite2D> _sprites;
     
     public override void _Input(InputEvent inputEvent)
     {
-        if (inputEvent is InputEventMouseButton mouseEvent)
+        if (inputEvent is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
         {
+            if (mouseEvent.ButtonIndex == MouseButton.Right)
+            {
+                _selected = null;
+                SelectionSprite.Visible = false;
+                return;
+            }
+
+            if (mouseEvent.ButtonIndex != MouseButton.Left) return;
+            
             // Convert mouse position to grid position
             var local = mouseEvent.Position - Position + new Vector2(Spacing * .5f, Spacing * .5f);
             GD.Print("mouse button event at ", local);
@@ -31,10 +42,24 @@ public partial class BoardNode : Node2D
             var x = (int)Math.Floor(local.X / Spacing);
             var y = (int)Math.Floor(local.Y / Spacing);
             GD.Print("mouse button event at {0} {1}", x, y);
-            // Swap pieces
-            
-            SelectionSprite.Position = new Vector2(x, y) * Spacing;
-            
+            if (_selected == null)
+            {
+                _selected = new Vector2I(x, y);
+                SelectionSprite.Position = new Vector2(x, y) * Spacing;
+                SelectionSprite.Visible = true;
+            }
+            else
+            { 
+                var target = new Vector2I(x, y);
+                _board.Swap(_selected.Value, target);
+                var sprite1 = _sprites[_selected.Value];
+                var sprite2 = _sprites[target];
+                sprite1.Position = IndexToPosition(target);
+                sprite2.Position = IndexToPosition(_selected.Value);
+                _sprites.Swap(_selected.Value, target); ;
+                _selected = null;
+                SelectionSprite.Visible = false;
+            }            
         }
     }
 
@@ -42,6 +67,9 @@ public partial class BoardNode : Node2D
     {
         _board = new Board(Width, Height);
         _sprites = new Array2D<Sprite2D>(Width, Height);
+     
+        _selected = null;
+        SelectionSprite.Visible = false;
         
         for (var x = 0; x < _board.Width; x++)
         {
@@ -57,4 +85,10 @@ public partial class BoardNode : Node2D
             }
         }
     }
+    
+    private Vector2 IndexToPosition(Vector2I index)
+    {
+        return new Vector2(index.X, index.Y) * Spacing;
+    }
+    
 }
