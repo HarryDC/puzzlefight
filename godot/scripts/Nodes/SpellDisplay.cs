132e134cafeff9fa8726536c43f37c993227b852
Harald Scheirich <hscheirich@yahoo.com>
2025-07-13 12:55:38 -0400

    Adds a rollover text to the spell button
    The user can now see the effects of a spell by
    rolling over the spell button

--- godot/scripts/Nodes/SpellDisplay.cs
+++ godot/scripts/Nodes/SpellDisplay.cs
@@ -3,14 +3,14 @@ using PuzzleFight.Spells;
 
 namespace PuzzleFight.Nodes;
 
-public partial class SpellDisplay : Node
+public partial class SpellDisplay : HBoxContainer
 {
     [Export] Label _label;
     [Export] TextureButton _button;
     
     private Spell _spell;
     private TextHandler _textHandler;
-
+    
     [Export]
     public Spell Spell
     {
@@ -24,6 +24,8 @@ public partial class SpellDisplay : Node
 
     [Export] public Participant Participant;
 
+    private Panel _rollover;
+    
     public override void _Ready()
     {
         _textHandler = GetNode<TextHandler>("/root/Game/TextHandler");
@@ -38,8 +40,34 @@ public partial class SpellDisplay : Node
     public override void _Process(double delta)
     {
         _button.Disabled = !_spell.CanCast(Participant.Character);
+
+        if (_rollover != null)
+        {
+            // Keep the rollover item away from the mouse otherwise we will 
+            // immediately receive a MousExited signal
+            _rollover.Position = GetGlobalMousePosition() + new Vector2(10,10);
+        }
     }
-    
+
+    public void OnMouseEntered()
+    {
+        _rollover = ResourceLoader.Load<PackedScene>("res://scenes/rollover_container.tscn").Instantiate<Panel>();
+        _rollover.Position = GetGlobalMousePosition() + new Vector2(10,10);
+        var label = _rollover.FindChild("RichTextLabel") as RichTextLabel;
+        if (label != null)
+        {
+            label.Text = Spell.Description;
+        }
+        GetTree().GetRoot().AddChild(_rollover);
+    }
+
+    public void OnMouseExited()
+    {
+        // There is some issue with remove child and de
+        //GetTree().GetRoot().RemoveChild(_rollover);
+        _rollover.QueueFree();
+        _rollover = null;
+    }
     
     
 }
